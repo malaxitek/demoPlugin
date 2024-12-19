39fb4ec8ba84f37b38ee67d614b3c07adfa22da6
1.exe <a.m.amm1928374655@gmail.com>
2024-12-19 22:11:00 +0300

    add lib

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,15 @@
 plugins {
     id ("java")
+    id ("org.jetbrains.intellij") version "1.17.4"
 }
 
 group = "com.demo.plugin"
 version = "1.0-SNAPSHOT"
 
-
+intellij {
+    version = "2024.1"
+    type = "IU"
+}
 
 repositories {
     mavenCentral()
--- src/main/java/demoPlugin.java
+++ src/main/java/demoPlugin.java
@@ -1,10 +1,18 @@
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.ui.Messages;
+import java.time.LocalTime;
 
 public class demoPlugin extends AnAction{
 
     @Override
     public void actionPerformed(AnActionEvent event) {
-        Messages.showMessageDialog();
+        LocalTime currentTime = LocalTime.now();
+
+        if (currentTime.getHour() == 14 && currentTime.getMinute() == 0) {
+            Messages.showMessageDialog("Пора на обед!", "Важно", Messages.getInformationIcon());
+        } else {
+            Messages.showMessageDialog("Текущее время: " + currentTime.toString(), "Важно", Messages.getInformationIcon());
+        }
     }
 }
